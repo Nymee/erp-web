595b29df1cc2b513c9e314b24b4518f98c4b17e7
Nayana <nayana@probeplus.in>
2025-09-05 17:10:11 +0530

    route navigation to login and token expiry check

--- src/route-layout/components/ProtectedRoute.tsx
+++ src/route-layout/components/ProtectedRoute.tsx
@@ -7,9 +7,19 @@ interface ProtectedRouteProps {
 const ProtectedRoute = ({ allowedRoles }: ProtectedRouteProps) => {
   const token = localStorage.getItem("decodedToken");
   const role = token ? JSON.parse(token).role : null;
+  const exp = token ? JSON.parse(token).exp : null;
+  let expired = false;
 
-  if (!token) {
-    return <Navigate to="/login" replace />;
+  if (exp) {
+    const currentTime = Date.now() / 1000;
+    expired = exp < currentTime;
+  }
+
+  console.log(token, "tokennnnn");
+
+  if (!token || expired === true) {
+    localStorage.removeItem("decodedToken");
+    return <Navigate to="/login" replace />; //rest of the code wont run once this is returned
   }
 
   if (role && allowedRoles.includes(role)) {
