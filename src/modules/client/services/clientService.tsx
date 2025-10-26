98519c0a624724fb02d8bc9ae69863357a0aa487
Nymee <nymmestic@gmail.com>
2025-10-26 23:24:01 +0530

    minot change

--- src/modules/client/services/clientService.tsx
+++ src/modules/client/services/clientService.tsx
@@ -6,7 +6,7 @@ const getClients = () => {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
 
     },
   })
