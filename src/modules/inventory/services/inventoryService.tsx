3a5f548489abdd4e5279c65a54d5f7fd2d1a265c
Nymee <86373532+Nymee@users.noreply.github.com>
2025-11-07 10:36:10 +0530

    testing commit

--- src/modules/inventory/services/inventoryService.tsx
+++ src/modules/inventory/services/inventoryService.tsx
@@ -24,7 +24,6 @@ export const getInventoryProducts = async (query: BasicQuery) => {
   });
   return response.data;
 };
-
 export const createInventory = async (data: any) => {
   try {
     const response = await apiClient.post(`/api/inventory`, data);
